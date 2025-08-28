32a6fb1f12c1ce64a07ff5bc85f3d3881fa8945a
Zannnn-Dev <radityaa8292@gmail.com>
2025-08-28 13:56:27 +0700

    products.js

--- products.js
+++ products.js
@@ -20,7 +20,7 @@ const products = {
             price: "Rp 35.000",
             description: "• RESELLER : 70K,PARTNER : 120K.",
             image: "https://raw.githubusercontent.com/NdikzDatabase/Database/main/Database/1756361241903-4qbruk.jpg",
-            checkoutUrl: "https://wa.me/6281802038115?text=Hay%20Min%2C%20saya%20ingin%20konfirmasi%20pembelian%20produk%20script%20nika%v14%20dengan%20harga%20Rp%2035.000.%20Berikut%20bukti%20pembayarannya."
+            checkoutUrl: "https://wa.me/6281802038115?text=Hay%20Min%2C%20saya%20ingin%20konfirmasi%20pembelian%20produk%20script%20nika%20v14%20dengan%20harga%20Rp%2035.000.%20Berikut%20bukti%20pembayarannya."
         },
     ],
     
